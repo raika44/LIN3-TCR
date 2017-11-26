275bb68e80e33461ee084a4e77853fa53892c5da
raika44 <33775480+raika44@users.noreply.github.com>
2017-11-26 17:22:14 +0700

    Update line-tcr2.py

--- line-tcr2.py
+++ line-tcr2.py
@@ -190,15 +190,15 @@ def bot(op):
 
         if op.type == 13:
             if op.param3 in mid:
-                if op.param2 in Amid or owner:
-                    G = Amid.getGroup(op.param1)
+                if op.param2 in Bots or owner:
+                    G = Bots.getGroup(op.param1)
                     G.preventJoinByTicket = False
                     Amid.updateGroup(G)
-                    Ticket = Amid.reissueGroupTicket(op.param1)
+                    Ticket = Bots.reissueGroupTicket(op.param1)
                     cl.acceptGroupInvitationByTicket(op.param1,Ticket)
                     G.preventJoinByTicket = True
                     Amid.updateGroup(G)
-                    Ticket = Amid.reissueGroupTicket(op.param1)
+                    Ticket = Bots.reissueGroupTicket(op.param1)
 
             if op.param3 in Amid:
                 if op.param2 in mid or owner:
