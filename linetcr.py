bdbb74a288f44a7d99a212cccf24f95555ebd42a
raika44 <33775480+raika44@users.noreply.github.com>
2017-11-26 10:16:22 +0700

    Update linetcr.py

--- linetcr.py
+++ linetcr.py
@@ -317,16 +317,16 @@ def bot(op):
                     elif wait["autoCancel"]["on"] == True:
                         if len(G.members) <= wait["autoCancel"]["members"]:
                            cl.rejectGroupInvitation(op.param1)
-             else:
-                 Inviter = op.param3.replace("",',')
-                 InviterX = Inviter.split(",")
-                 matched_list = []
-                 for tag in wait["blacklist"]:
-                     matched_list+=filter(lambda str: str == tag, InviterX)
-                 if matched_list == []:
-                     pass
-                 else:
-                     cl.cancelGroupInvitation(op.param1, matched_list)
+            else:
+                Inviter = op.param3.replace("",',')
+                InviterX = Inviter.split(",")
+                matched_list = []
+                for tag in wait["blacklist"]:
+                    matched_list+=filter(lambda str: str == tag, InviterX)
+                if matched_list == []:
+                    pass
+                else:
+                    cl.cancelGroupInvitation(op.param1, matched_list)
                     
         #------Joined User Kick start------#
         if op.type == 17:
