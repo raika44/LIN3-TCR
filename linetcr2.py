aec6cc41577463d4c487d73f23315c345ee89e1c
raika44 <33775480+raika44@users.noreply.github.com>
2017-11-28 16:19:29 +0700

    Update linetcr2.py

--- linetcr2.py
+++ linetcr2.py
@@ -95,8 +95,7 @@ Hmid = ke.getProfile().mid
 Imid = ku.getProfile().mid
 
 Bots=[mid,Amid,Bmid,Cmid,Dmid,Emid,Fmid,Gmid,Hmid,Imid,"u5427d8047ab127f5e237eaedd1f0b93b"]
-admin=["u5427d8047ab127f5e237eaedd1f0b93b","u1fa69c99e0aa30b9977be7639fb76cd2"]
-owner=["u5427d8047ab127f5e237eaedd1f0b93b"]
+admin=["u5427d8047ab127f5e237eaedd1f0b93b"]
 creator=["u5427d8047ab127f5e237eaedd1f0b93b"]
 wait = {
     'contact':True,
